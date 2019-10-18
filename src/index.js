545831799178494f2f212a995e9e1de1113e723b
DevArslan <arslan.gareev.2017@gmail.com>
2019-10-18 18:42:45 +0300

    task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+  let tempBrackets = [];
+
+  if (str.length % 2 != 0 ){
+    return false
+  }
+
+  for(let i = 0; i < str.length; i++){
+    for(let k = 0; k < bracketsConfig.length; k++){
+      if(str[i] == bracketsConfig[k][1] && tempBrackets[tempBrackets.length-1] == bracketsConfig[k][0]){
+        tempBrackets.pop()
+      }
+
+      else if(str[i] == bracketsConfig[k][0] || str[i] == bracketsConfig[k][1]){
+        tempBrackets.push(str[i])
+      }
+    }
+  }
+  if(tempBrackets.length == 0){
+    return true
+  }
+  else{
+    return false
+  }
+
+
+
 }
